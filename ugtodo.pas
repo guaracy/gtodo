438b189b9fde5a83aba0da3c971dc8392f2a4526
Guaracy <guaracy.bm@gmail.com>
2022-03-09 20:54:16 -0300

    initial commit

---
+++ ugtodo.pas
@@ -0,0 +1,126 @@
+unit ugtodo;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+  Classes, SysUtils, Forms, Controls, Graphics, Dialogs, StdCtrls, ExtCtrls,
+  Grids, process, LCLType;
+
+type
+
+  { TForm1 }
+
+  TForm1 = class(TForm)
+    edTarefa: TLabeledEdit;
+    Panel1: TPanel;
+    sgTarefas: TStringGrid;
+    procedure edTarefaKeyPress(Sender: TObject; var Key: char);
+    procedure FormCreate(Sender: TObject);
+    procedure sgTarefasDblClick(Sender: TObject);
+    procedure sgTarefasPrepareCanvas(sender: TObject; aCol, aRow: Integer;
+      aState: TGridDrawState);
+  private
+    tDir,
+    cDir : string;
+    chgfn,
+    todofn:string;
+    notGit:Boolean;
+
+  public
+
+  end;
+
+var
+  Form1: TForm1;
+const
+  todoname = 'gtodo.txt';
+  changelog = 'changelog.txt';
+
+implementation
+
+{$R *.lfm}
+
+{ TForm1 }
+
+procedure TForm1.FormCreate(Sender: TObject);
+var
+  sOut: string;
+begin
+  cDir:=IncludeTrailingPathDelimiter(GetCurrentDir);
+  todofn:=cDir+todoname;
+  chgfn:=cDir+changelog;
+  notGit:=True;
+  sgTarefas.SaveOptions:=soAll;
+  if FileExists(todofn) then begin
+    sgTarefas.LoadFromFile(todofn);
+  end else
+    sgTarefas.RowCount:=1;
+  sgTarefas.Options:=sgTarefas.Options+[goFixedRowNumbering,goRowSelect];
+  if not RunCommandIndir(cDir,'git',['rev-parse','--show-toplevel'],sOut,[poWaitOnExit,poStderrToOutPut,poNoConsole]) then begin
+    Caption := 'gtodo : (LOCAL) - '+cDir;
+    exit;
+  end;
+  notGit:=False;
+  tDir := Trim(sOut);
+  Caption := 'gtodo : (GIT) - '+tDir;
+
+end;
+
+procedure TForm1.sgTarefasDblClick(Sender: TObject);
+var
+  lst: TStringList;
+  sOut: string;
+   s: String;
+begin
+  if sgTarefas.RowCount=1 then exit;
+  s:=sgTarefas.Cells[1,sgTarefas.Row];
+  if MessageDlg('Commit', s+#10#13'Do you wish to Commit?', mtConfirmation, [mbYes, mbNo],0) = mrYes then begin
+    lst := TStringList.Create();
+    if FileExists(chgfn) then
+      lst.LoadFromFile(chgfn);
+    lst.Insert(0,FormatDateTime('YYYYMMDD : ',now)+s);
+    lst.SaveToFile(chgfn);
+    lst.Free;
+  end else exit;
+  sgTarefas.DeleteRow(sgTarefas.Row);
+  sgTarefas.SaveToFile(todofn);
+  if notGit then exit;
+  if not RunCommandInDir(cDir,'git',['add','.'],sOut,[poWaitOnExit,poStderrToOutPut,poNoConsole]) then begin
+    ShowMessage('ERROR : git add .'+sOut);
+    exit;
+  end;
+  if not RunCommandInDir(cDir,'git',['commit','-m',s],sOut,[poWaitOnExit,poStderrToOutPut,poNewConsole{poNoConsole}]) then begin
+    ShowMessage('ERROR : git commit -m '+QuotedStr(s)+sOut);
+    exit;
+  end;
+  ShowMessage('COMMIT OK'#10#13+sOut);
+end;
+
+procedure TForm1.sgTarefasPrepareCanvas(sender: TObject; aCol, aRow: Integer;
+  aState: TGridDrawState);
+begin
+  if (aRow < 1) or (aCol < 1) then exit;
+  if gdSelected in aState then exit;
+  case sgTarefas.Cells[1,aRow][1] of
+    '-' : sgTarefas.Canvas.Font.Color:=clGreen;
+    '+' : sgTarefas.Canvas.Font.Color:=clRed;
+  end;
+end;
+
+procedure TForm1.edTarefaKeyPress(Sender: TObject; var Key: char);
+var
+  s: string;
+begin
+  if Key <> #13 then exit;
+  s:=Trim(edTarefa.Text);
+  if s='' then exit;
+  sgTarefas.RowCount:=sgTarefas.RowCount+1;
+  sgTarefas.Cells[1,sgTarefas.RowCount-1]:=s;
+  sgTarefas.SaveToFile(todofn);
+  edTarefa.Text:='';
+end;
+
+end.
+
